ea8d31e2530aa5246737e18e55b7ce2bcc938474
Kiril bc2025-6 <fedotovpoliyt@gmail.com>
2025-11-13 19:38:27 +0200

    Initial commit - update main.js and .gitignore

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { program } = require('commander');
+const { Command } = require('commander');
 const http = require('http');
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
-const superagent = require('superagent');
+
+const program = new Command();
 
 program
   .requiredOption('-H, --host <host>', 'адреса сервера')
@@ -12,3 +13,19 @@ program
 program.parse(process.argv);
 
 const options = program.opts();
+
+//створення директорії кешу якщо її нема
+if (!fs.existsSync(options.cache)) {
+  fs.mkdirSync(options.cache, { recursive: true });
+  console.log(`Директорію кеша створено: ${options.cache}`);
+}
+//кріейт
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Сервіс інвентаризації працює');
+});
+//запуск
+server.listen(options.port, options.host, () => {
+  console.log(`Сервер працює на http://${options.host}:${options.port}/`);
+  console.log(`Директорія кешу: ${options.cache}`);
+});
